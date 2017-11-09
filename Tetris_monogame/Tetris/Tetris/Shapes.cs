e94fd6f024f5a85b20234791c0fd5c30c7bd44a9
ginoesco <g.escolano91@gmail.com>
2017-11-08 23:07:59 -0800

    Add files via upload

---
+++ Tetris_monogame/Tetris/Tetris/Shapes.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Tetris
+{
+    class Shapes
+    {
+        List<int[,]> ShapeList = new List<int[,]>();
+        List<Color> ColorList = new List<Color>(); 
+        const int pixelWidth = 31;
+        const int pixelLength = 32; 
+
+        public Shapes()
+        {
+            ColorList.Add(Color.DarkOrange);
+            ColorList.Add(Color.Cyan);
+            ColorList.Add(Color.Yellow);
+            ColorList.Add(Color.Blue);
+            ColorList.Add(Color.MediumVioletRed);
+            ColorList.Add(Color.LightGreen);
+            ColorList.Add(Color.Purple);
+
+
+            //T shape
+            ShapeList.Add(new int[4, 4]{{0,1,0,0},
+                                        {1,1,1,0},
+                                        {0,0,0,0},
+                                        {0,0,0,0} });
+            //Z shape
+            ShapeList.Add(new int[4, 4]{{1,1,0,0},
+                                        {0,1,1,0},
+                                        {0,0,0,0},
+                                        {0,0,0,0} });
+
+            //S shape
+            ShapeList.Add(new int[4, 4]{{0,0,1,1},
+                                        {0,1,1,0},
+                                        {0,0,0,0},
+                                        {0,0,0,0} });
+            //L Shape
+            ShapeList.Add(new int[4, 4]{{0,1,0,0},
+                                        {0,1,0,0},
+                                        {0,1,1,0},
+                                        {0,0,0,0} });
+
+            //J Shape
+            ShapeList.Add(new int[4, 4]{{0,0,0,0},
+                                        {1,0,0,0},
+                                        {1,1,1,0},
+                                        {0,0,0,0} });
+            //Square Shape
+            ShapeList.Add(new int[4, 4]{{0,0,0,0},
+                                        {0,1,1,0},
+                                        {0,1,1,0},
+                                        {0,0,0,0} });
+            //Line Shape
+            ShapeList.Add(new int[4, 4]{{0,1,0,0},
+                                        {0,1,0,0},
+                                        {0,1,0,0},
+                                        {0,1,0,0} });
+        }
+
+        public List<int[,]> GetShapeList()
+        {
+            return ShapeList;
+        }
+
+        public List<Color> GetColorList()
+        {
+            return ColorList; 
+        }
+    }
+}
---
+++ Tetris_monogame/Tetris/Tetris/TetrisGame.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System; 
+namespace Tetris
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class TetrisGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Shapes shapeObj = new Shapes();
+        Random rnd = new Random(); 
+
+        private Texture2D block;
+        const int pixelWidth = 32;
+        const int pixelLength = 31; 
+        int[,] shape = new int[4, 4] { { 0, 1, 0, 0 },
+                                       { 1, 1, 1, 0 },
+                                       { 0, 0, 0, 0},
+                                       { 0, 0, 0, 0 } };
+        
+        int[,] Zshape = new int[4, 4] { { 1, 1, 0, 0 },
+                                        { 0, 1, 1, 0 },
+                                        { 0, 0, 0, 0},
+                                        { 0, 0, 0, 0 } };
+
+        int[,] Square = new int[4, 4] {{ 0, 0, 0, 0 },
+                                       { 0, 1, 1, 0 },
+                                       { 0, 1, 1, 0},
+                                       { 0, 0, 0, 0 } };
+
+        int[,] Line = new int[4, 4] {  { 0, 1, 0, 0 },
+                                       { 0, 1, 0, 0 },
+                                       { 0, 1, 0, 0},
+                                       { 0, 1, 0, 0 } };
+
+        int[,] Jshape = new int[4, 4] {{ 0, 0, 0, 0 },
+                                       { 1, 0, 0, 0 },
+                                       { 1, 1, 1, 0},
+                                       { 0, 0, 0, 0 } };
+
+        int[,] Lshape = new int[4, 4] {{ 0, 0, 0, 0 },
+                                       { 0, 1, 0, 0 },
+                                       { 0, 1, 0, 0},
+                                       { 0, 1, 1, 0 } };
+        public TetrisGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            block = Content.Load<Texture2D>("block"); 
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+            block.Dispose(); 
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            List<int[,]> shapeList = shapeObj.GetShapeList();
+            List<Color> Colors = shapeObj.GetColorList(); 
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            int posX = 200;
+            int posY = 200;
+            int rnum = rnd.Next(0, 6); 
+            //int rnum = 6; 
+
+            shape = shapeList[rnum]; 
+
+            for(int i = 0; i<4; i++)
+            {
+                for(int k = 0; k<4; k++)
+                {
+                    if(shape[k,i] == 1)
+                    {
+
+                        spriteBatch.Draw(block, new Vector2(posX+i*pixelWidth, posY+k*pixelLength), Colors[rnum]);
+                    }
+                }
+            }
+            spriteBatch.End(); 
+
+            base.Draw(gameTime);
+        }
+    }
+}
